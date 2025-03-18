ee3a0895e5ca1cae66b5037f1397c2760d19fdc4
TacoL <34144367+TacoL@users.noreply.github.com>
2025-03-18 17:12:29 -0400

    fixed errors

--- STEVAL-FCU001V2/Debug/Middlewares/HCI/Controller/bluenrg_gatt_aci.d
+++ STEVAL-FCU001V2/Debug/Middlewares/HCI/Controller/bluenrg_gatt_aci.d
@@ -1,56 +1,38 @@
 Middlewares/HCI/Controller/bluenrg_gatt_aci.o: \
  ../Middlewares/HCI/Controller/bluenrg_gatt_aci.c \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hal_types.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/osal.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/ble_status.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hal.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hci_const.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/compiler.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hal_types.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/link_layer.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hci.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/ble_list.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_aci_const.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hci_const.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gatt_server.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/ble_status.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gatt_aci.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gatt_server.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gap.h \
- C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/link_layer.h
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hal_types.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/osal.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/ble_status.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hal.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hci_const.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/compiler.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hal_types.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/link_layer.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hci.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/ble_list.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_aci_const.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/hci_const.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gatt_server.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/ble_status.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gatt_aci.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gatt_server.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/bluenrg_gap.h:
-
-C:/Users/gmessina/OneDrive\ -\ STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include/link_layer.h:
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hal_types.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/osal.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/ble_status.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hal.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hci_const.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/compiler.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hal_types.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/link_layer.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hci.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/ble_list.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_aci_const.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hci_const.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gatt_server.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/ble_status.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gatt_aci.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gatt_server.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gap.h \
+ /Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/link_layer.h
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hal_types.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/osal.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/ble_status.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hal.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hci_const.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/compiler.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hal_types.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/link_layer.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hci.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/ble_list.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_aci_const.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/hci_const.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gatt_server.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/ble_status.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gatt_aci.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gatt_server.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/bluenrg_gap.h:
+/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include/link_layer.h:
--- STEVAL-FCU001V2/Debug/Middlewares/HCI/subdir.mk
+++ STEVAL-FCU001V2/Debug/Middlewares/HCI/subdir.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -15,6 +15,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Middlewares/HCI/hci.o: ../Middlewares/HCI/hci.c Middlewares/HCI/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F401xC -DDEBUG -c -I../Drivers/CMSIS/Include -I../Drivers/BSP/Components/lsm6dsr -I../Drivers/BSP/Components/lps22hh -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../X-CUBE-MEMS1/Target -I"C:/Users/gmessina/OneDrive - STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Include" -I"C:/Users/gmessina/OneDrive - STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Drivers/BSP/Components/BLE" -I"C:/Users/gmessina/OneDrive - STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/HCI" -I"C:/Users/gmessina/OneDrive - STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/HCI/Controller" -I"C:/Users/gmessina/OneDrive - STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Interface" -I"C:/Users/gmessina/OneDrive - STMicroelectronics/Drones/Development/Source_Code/STEVALFCU01V2/Middlewares/Utils" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Middlewares/HCI/hci.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Middlewares/HCI/%.o Middlewares/HCI/%.su Middlewares/HCI/%.cyclo: ../Middlewares/HCI/%.c Middlewares/HCI/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F401xC -DDEBUG -c -I../Drivers/CMSIS/Include -I../Drivers/BSP/Components/lsm6dsr -I../Drivers/BSP/Components/lps22hh -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../X-CUBE-MEMS1/Target -I"/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Include" -I"/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Drivers/BSP/Components/BLE" -I"/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/HCI" -I"/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/HCI/Controller" -I"/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Interface" -I"/Users/sagehere/Documents/373Project/STEVAL-FCU001V2/Middlewares/Utils" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Middlewares-2f-HCI
+
+clean-Middlewares-2f-HCI:
+	-$(RM) ./Middlewares/HCI/hci.cyclo ./Middlewares/HCI/hci.d ./Middlewares/HCI/hci.o ./Middlewares/HCI/hci.su
+
+.PHONY: clean-Middlewares-2f-HCI
 
